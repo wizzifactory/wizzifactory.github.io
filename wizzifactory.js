8203a365c22b12342e23a0b89904fa6d2654049b
wizziteam <wizzi.team@gmail.com>
2015-09-25 20:17:50 +0200

    step 2

--- wizzifactory.js
+++ wizzifactory.js
@@ -1,4 +1,4 @@
-// generator: wizzi-lab-artifatcs/lib/js/module/gen/main.js, utc time: Fri, 25 Sep 2015 13:43:18 GMT
+// generator: wizzi-lab-artifatcs/lib/js/module/gen/main.js, utc time: Fri, 25 Sep 2015 18:00:33 GMT
 (function() {
     if (document.addEventListener) {
         
